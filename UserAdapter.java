0a76e4345401ac9b1f7f9e4453deb17a0adfabc8
JOEAI <42526993+jomarbp@users.noreply.github.com>
2025-02-18 16:24:34 -0500

    Create UserAdapter.java

---
+++ UserAdapter.java
@@ -0,0 +1,39 @@
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+    private List<User> userList;
+
+    public UserAdapter(List<User> userList) {
+        this.userList = userList;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.user_item, parent, false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        User user = userList.get(position);
+        holder.textViewName.setText(user.getName());
+        holder.textViewEmail.setText(user.getEmail());
+        holder.textViewPhone.setText(user.getPhone());
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    static class UserViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewName, textViewEmail, textViewPhone;
+
+        UserViewHolder(View itemView) {
+            super(itemView);
+            textViewName = itemView.findViewById(R.id.textViewName);
+            textViewEmail = itemView.findViewById(R.id.textViewEmail);
+            textViewPhone = itemView.findViewById(R.id.textViewPhone);
+        }
+    }
+}
